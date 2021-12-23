72546462b612e6d7b1d44cb0d72a53d7c67f68a5
kamildoman <domanweb1@gmail.com>
2021-12-23 12:32:08 +0100

    edit settings2

--- demoty/settings.py
+++ demoty/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'media')
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
